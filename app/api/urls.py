ac9bb500e757b2e46b5bd18ea974dac19bba485c
Augusto Pontes <augustopontes010@gmail.com>
2026-05-19 01:30:20 -0300

    fix(urls): Added right url path for the payments

--- app/api/urls.py
+++ app/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from app.api import views
 
 urlpatterns = [
-    path("payments", views.PaymentCreateView.as_view(), name="payment-create"),
+    path("payments/", views.PaymentCreateView.as_view(), name="payment-create"),
 ]
